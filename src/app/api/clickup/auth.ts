b00d163bb0710ea86300cb4aade458765ebe8798
tiojimbo <isamuelmacedo0@gmail.com>
2025-06-07 23:06:55 -0300

    Correção

--- src/app/api/clickup/auth.ts
+++ src/app/api/clickup/auth.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const clientId = process.env.CLICKUP_CLIENT_ID!;
-  const redirectUri = process.env.NEXT_PUBLIC_CLICKUP_REDIRECT_URI!;
+  const redirectUri = process.env.CLICKUP_REDIRECT_URI!;
   const authUrl = `https://app.clickup.com/api?client_id=${clientId}&redirect_uri=${redirectUri}`;
   return NextResponse.redirect(authUrl);
 }
